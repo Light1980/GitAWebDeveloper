83f06932a6dd917ebdc80dbfee55f3b406d411eb
Light1980 <pxc1861@hotmail.com>
2017-04-25 00:18:24 +0800

    update CSS WORKFLOW coursework

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+cssnested = require('postcss-nested');
+
+gulp.task('default', function() {
+	console.log("Hi, here is your first gulp task.")
+})
+
+gulp.task('html', function() {
+	console.log('Ok, here is the html task.')
+})
+
+gulp.task('styles', function() {
+	return gulp.src('app/assets/styles/*.css')
+		.pipe(postcss([autoprefixer,cssvars, cssnested]))
+		.pipe(gulp.dest('app/temp/styles'))
+})
+
+// gulp-watch
+
+gulp.task('watch', function() {
+	watch('app/index.html', function () {
+		gulp.start('html');
+	})
+
+	watch('app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	})
+})
