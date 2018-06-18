2b64d422ce2fd5d2f1b674ad4a707d607d22b689
彭津雲 <pxc1861@hotmail.com>
2018-06-19 04:24:02 +0800

    Section 6

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,7 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-cssnested = require('postcss-nested'),
-cssImport = require('postcss-import')
+require('./gulp/tasks/styles');
 
-gulp.task('default', function() {
-	console.log("Hi, here is your first gulp task.")
-})
+require('./gulp/tasks/watch');
 
-gulp.task('html', function() {
-	console.log('Ok, here is the html task.')
-})
 
-gulp.task('styles', function() {
-	return gulp.src('app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, autoprefixer,cssvars, cssnested]))
-		.pipe(gulp.dest('app/temp/styles'))
-})
 
-// gulp-watch
 
-gulp.task('watch', function() {
-	watch('app/index.html', function () {
-		gulp.start('html');
-	})
-
-	watch('app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	})
-})
